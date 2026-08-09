01660848739a3b3d06067086d2bdf3b190a36494
gururani-abhishek <gururani.abhishek@gmail.com>
2026-08-09 13:59:12 +0530

    correct binary search code

--- LC Solutions/binary_search.java
+++ LC Solutions/binary_search.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
+
 public class binary_search {
     public static void main(String[] args) {
-        int[] nums = new int[6];
-        for (int idx = 1; idx < 6; idx++) {
-            nums[idx] = idx * 2;
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[14];
+        for (int idx = 0; idx < 14; idx++) {
+            nums[idx] = idx+1;
         }
         int len = nums.length;
         int left = 0, right = len - 1;
-        int target = 3;
-        while (left < right) {
+        int target = 13;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
+            System.out.println("mid element is : " + nums[mid] + " cut at " + (mid+1));
+            sc.nextLine();
             if (nums[mid] == target) {
-                System.out.println(mid);
                 break;
             } else if (nums[mid] > target) {
                 right = mid - 1;
